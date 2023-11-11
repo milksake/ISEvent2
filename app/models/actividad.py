34a51eb35019a8cdb2da7d7f739d084711b18d69
flechita-veloz <79392249+flechita-veloz@users.noreply.github.com>
2023-11-10 22:30:29 -0500

    Add files via upload

---
+++ app/models/actividad.py
@@ -0,0 +1,9 @@
+class Actividad(self,id,nombre,fecha_inicio, fecha_fin,tipo_actividad, img_actividad,descripcion_actividad):
+    self.idActividad= id
+    self.nombre= nombre
+    self.fecha_inicio= fecha_inicio
+    self.fecha_fin= fecha_fin
+    self.img_actividad= img_actividad
+    self.descripcion_actividad= descripcion_actividad
+
+
