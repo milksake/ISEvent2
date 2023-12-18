a4621fca87f0829febec1168cf0a8f61ecdb5055
KuliesJ <jorge.nunez.paucar@ucsp.edu.pe>
2023-12-18 18:12:46 -0500

    El formulario acepta espacios y tildes

--- app/forms/FormValidarActividad.py
+++ app/forms/FormValidarActividad.py
@@ -4,26 +4,26 @@ from wtforms.validators import DataRequired, Length, Regexp, ValidationError
 
 class FormValidarActividad(FlaskForm):
     nombre = StringField('Nombre', validators=[DataRequired(), Length(min=5, max=100, message="La longitud debe estar entre 5 y 100 caracteres."),
-                        Regexp('^[a-zA-Z0-9.,-_]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión y subguión.")])
+                        Regexp('^[a-zA-Z0-9.,\-_ áéíóúÁÉÍÓÚ]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión, subguión y tildes.")])
 
     fechaInicio = DateTimeLocalField('Fecha de inicio', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
 
     fechaFin = DateTimeLocalField('Fecha de fin', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
 
     tipo = StringField('Tipo de actividad', validators=[Length(min=1, max=20, message="La longitud debe estar entre 1 y 20 caracteres."),
-                        Regexp('^[a-zA-Z0-9.,-_]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión y subguión.")])
+                        Regexp('^[a-zA-Z0-9.,\-_ áéíóúÁÉÍÓÚ]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión, subguión y tildes.")])
 
     descripcion = TextAreaField('Descripción', validators=[Length(min=20, max=200, message="La longitud debe estar entre 20 y 200 caracteres."),
-                        Regexp('^[a-zA-Z0-9.,-_]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión y subguión.")])
+                        Regexp('^[a-zA-Z0-9.,\-_ áéíóúÁÉÍÓÚ]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión, subguión y tildes.")])
 
     evento = StringField('Evento', validators=[
         Length(min=1, max=40, message="La longitud debe estar entre 1 y 20 caracteres."),
-        Regexp('^[a-zA-Z0-9.,_ -]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión, subguión y espacio.")
+        Regexp('^[a-zA-Z0-9.,\-_ áéíóúÁÉÍÓÚ]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión, subguión y tildes.")
     ])
 
     ambiente= StringField('Ambiente', validators=[
         Length(min=1, max=20, message="La longitud debe estar entre 1 y 20 caracteres."),
-        Regexp('^[a-zA-Z0-9.,_ -]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión, subguión y espacio.")
+        Regexp('^[a-zA-Z0-9.,\-_ áéíóúÁÉÍÓÚ]*$', message="Solo se permiten caracteres alfanuméricos, coma, punto, guión, subguión y tildes.")
     ])
 
     def validate(self):
